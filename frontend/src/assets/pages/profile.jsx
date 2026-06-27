50e51619fa3973d7d4fbee599b2f079a6a2b4c7c
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-27 13:51:19 +0100

    style: update button icon color and remove unnecessary padding in Profile component

--- frontend/src/assets/pages/profile.jsx
+++ frontend/src/assets/pages/profile.jsx
@@ -74,11 +74,11 @@ const Profile
     ];
     return (
       <>
-        <div className=" bg-black/90 backdrop-blur-md p-4 min-h-screen max-w-md mx-auto flex text-white overflow-y-auto flex-col ">
+        <div className=" bg-black/90 backdrop-blur-md min-h-screen max-w-md mx-auto flex text-white overflow-y-auto flex-col ">
           {/* Header */}
           <div className="flex items-center justify-between px-5 pt-6 pb-4">
             <button onClick={() => navigate(-1)} className="p-2 rounded-full hover:bg-gray-100 transition">
-              <ArrowLeft className="w-5 h-5 text-gray-800" />
+              <ArrowLeft className="w-5 h-5 text-gray-200" />
             </button>
             <h1 className="text-lg font-extrabold text-gray-300">My Profile</h1>
             <div className="w-9" />
