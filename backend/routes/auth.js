69af7b0a69d330e63386b71c9c64d3d1bd8b43cb
DanielApple05 <ejimofordanielchukwuebuka@gmail.com>
2026-06-30 02:31:39 +0100

    fixing API route

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -217,12 +217,16 @@ router.put('/restore-default-pin', protect, async (req, res) => {
 
   try {
     const user = await User.findById(req.user.id);
+
     if (!user) {
       return res.status(404).json({
         message: 'User not found'
       });
     }
-    if (confirmEmail !== user.email) {
+    if (
+      confirmEmail?.trim().toLowerCase() !==
+      user.email.toLowerCase()
+    ) {
       return res.status(400).json({
         message: 'Invalid email'
       });
@@ -232,9 +236,11 @@ router.put('/restore-default-pin', protect, async (req, res) => {
     return res.status(200).json({
       message: 'PIN removed successfully'
     });
-
   } catch (err) {
-    res.status(500).json({ message: 'Server error' });
+    console.error(err);
+    return res.status(500).json({
+      message: 'Server error'
+    });
   }
 });
 
